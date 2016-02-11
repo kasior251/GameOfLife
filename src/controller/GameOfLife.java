ba477219c6cd1a39c37668e84a0ce819da634b00
Benjamin B <s305338@stud.hioa.no>
2016-02-11 13:49:39 +0100

    - Refactored & restructured project to fit our initial UML diagram

---
+++ src/controller/GameOfLife.java
@@ -0,0 +1,27 @@
+package controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class GameOfLife {
+
+    @FXML private Button testbutton;
+    @FXML private Text testtext;
+    @FXML private Canvas gridcanvas;
+
+
+    @FXML
+    protected void handleSubmitButtonAction(MouseEvent event) {
+
+    }
+
+
+    @FXML protected void handleStepButton(ActionEvent event) {
+    }
+
+}
---
+++ src/model/GameBoard.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Benjamin on 25.01.2016.
+ */
+public class GameBoard {
+
+}

f2a8189b5697076535513ae1b7b27683c6bb615a
Benjamin B <s305338@stud.hioa.no>
2016-02-24 15:22:36 +0100

    - Fixed stupid commit

--- src/model/GameBoard.java
+++ src/model/GameBoard.java
@@ -6,96 +6,105 @@ public class GameBoard {
     private int columns;
     private int rows;
 
+    public GameBoard() {
+        board = new boolean[100][150];
+
+        this.columns = 100;
+        this.rows = 150;
+
+
+        board[13][18] = true;
+        board[14][18] = true;
+        board[14][19] = true;
+        board[14][20] = true;
+        board[15][21] = true;
+        board[16][20] = true;
+        board[16][21] = true;
+
+        board[13][34] = true;
+        board[14][34] = true;
+        board[14][33] = true;
+        board[14][32] = true;
+        board[15][31] = true;
+        board[16][32] = true;
+        board[16][31] = true;
+
+        board[24][22] = true;
+        board[24][23] = true;
+        board[24][24] = true;
+        board[24][28] = true;
+        board[24][29] = true;
+        board[24][30] = true;
+
+        board[25][22] = true;
+        board[25][25] = true;
+        board[25][27] = true;
+        board[25][30] = true;
+
+        board[26][22] = true;
+        board[26][30] = true;
+
+        board[28][23] = true;
+        board[28][29] = true;
+
+        board[29][24] = true;
+        board[29][25] = true;
+        board[29][27] = true;
+        board[29][28] = true;
+
+        board[38][20] = true;
+        board[38][21] = true;
+        board[39][20] = true;
+        board[39][21] = true;
+        board[38][32] = true;
+        board[38][31] = true;
+        board[39][32] = true;
+        board[39][31] = true;
+
+        board[48][24] = true;
+        board[49][24] = true;
+        board[48][28] = true;
+        board[49][28] = true;
+
+        board[52][21] = true;
+        board[52][22] = true;
+        board[52][24] = true;
+        board[52][28] = true;
+        board[52][30] = true;
+        board[52][31] = true;
+
+        board[53][22] = true;
+        board[53][23] = true;
+        board[53][24] = true;
+        board[53][28] = true;
+        board[53][29] = true;
+        board[53][30] = true;
+
+        board[54][23] = true;
+        board[54][29] = true;
+
+        board[61][21] = true;
+        board[61][31] = true;
+        board[61][20] = true;
+        board[61][32] = true;
+
+        board[62][21] = true;
+        board[62][31] = true;
+
+        board[63][18] = true;
+        board[63][19] = true;
+        board[63][20] = true;
+        board[63][32] = true;
+        board[63][33] = true;
+        board[63][34] = true;
+
+        board[64][34] = true;
+        board[64][18] = true;
+
+    }
+
     public GameBoard(int columns, int rows) {
         board = new boolean[columns][rows];
-
-        board[1+12][1+17] = true;
-        board[2+12][1+17] = true;
-        board[2+12][2+17] = true;
-        board[2+12][3+17] = true;
-        board[3+12][4+17] = true;
-        board[4+12][3+17] = true;
-        board[4+12][4+17] = true;
-
-        board[1+12][17+17] = true;
-        board[2+12][17+17] = true;
-        board[2+12][16+17] = true;
-        board[2+12][15+17] = true;
-        board[3+12][14+17] = true;
-        board[4+12][15+17] = true;
-        board[4+12][14+17] = true;
-
-        board[12+12][5+17] = true;
-        board[12+12][6+17] = true;
-        board[12+12][7+17] = true;
-        board[12+12][11+17] = true;
-        board[12+12][12+17] = true;
-        board[12+12][13+17] = true;
-
-        board[13+12][5+17] = true;
-        board[13+12][8+17] = true;
-        board[13+12][10+17] = true;
-        board[13+12][13+17] = true;
-
-        board[14+12][5+17] = true;
-        board[14+12][13+17] = true;
-
-        board[16+12][6+17] = true;
-        board[16+12][12+17] = true;
-
-        board[17+12][7+17] = true;
-        board[17+12][8+17] = true;
-        board[17+12][10+17] = true;
-        board[17+12][11+17] = true;
-
-        board[26+12][3+17] = true;
-        board[26+12][4+17] = true;
-        board[27+12][3+17] = true;
-        board[27+12][4+17] = true;
-        board[26+12][15+17] = true;
-        board[26+12][14+17] = true;
-        board[27+12][15+17] = true;
-        board[27+12][14+17] = true;
-
-        board[36+12][7+17] = true;
-        board[37+12][7+17] = true;
-        board[36+12][11+17] = true;
-        board[37+12][11+17] = true;
-
-        board[40+12][4+17] = true;
-        board[40+12][5+17] = true;
-        board[40+12][7+17] = true;
-        board[40+12][11+17] = true;
-        board[40+12][13+17] = true;
-        board[40+12][14+17] = true;
-
-        board[41+12][5+17] = true;
-        board[41+12][6+17] = true;
-        board[41+12][7+17] = true;
-        board[41+12][11+17] = true;
-        board[41+12][12+17] = true;
-        board[41+12][13+17] = true;
-
-        board[42+12][6+17] = true;
-        board[42+12][12+17] = true;
-
-        board[49+12][4+17] = true;
-        board[49+12][14+17] = true;
-        board[49+12][3+17] = true;
-        board[49+12][15+17] = true;
-
-        board[50+12][4+17] = true;
-        board[50+12][14+17] = true;
-
-        board[51+12][1+17] = true;
-        board[51+12][2+17] = true;
-        board[51+12][3+17] = true;
-        board[51+12][15+17] = true;
-        board[51+12][16+17] = true;
-        board[51+12][17+17] = true;
-
-        board[52+12][17+17] = true;
-        board[52+12][1+17] = true;
 
         this.columns = columns;
         this.rows = rows;
@@ -103,6 +112,8 @@ public class GameBoard {
 
     public void setBoard(boolean[][] board) {
         this.board = board;
+        this.columns = board.length;
+        this.rows = board[0].length;
     }
 
     public int getRows() {
@@ -120,6 +131,15 @@ public class GameBoard {
     public void setCell(int x, int y, boolean state) {
         board[x][y] = state;
     }
+
+    public boolean getCell(int x, int y) {
+        return board[x][y];
+    }
+
+    public void toggleCell(int x, int y) {
+        board[x][y] = !board[x][y];
+    }
+
 
     public int countNeighbours(int x, int y) {
         int count = 0;
